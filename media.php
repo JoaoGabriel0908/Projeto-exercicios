f879a965dc4e2c076cd6f0d8f73a95f7a64a6b91
João Gabriel <joaogabrielc700@gmail.com>
2022-02-14 10:36:26 -0300

    Exercicio

--- media.php
+++ media.php
@@ -20,7 +20,7 @@ if (isset($_POST['btncalc'])) {
     $nota2 = $_POST['txtn2'];
     $nota3 = $_POST['txtn3'];
     $nota4 = $_POST['txtn4'];
-    
+
     // Validação de tratamento de erro para caixa vazia
     if ($_POST['txtn1'] == "" || $_POST['txtn2'] == "" || $_POST['txtn3'] == "" || $_POST['txtn4'] == "")
         echo (ERRO_MSG_CAIXA_VAZIA);
@@ -42,51 +42,91 @@ if (isset($_POST['btncalc'])) {
 
 <head>
     <title>Média</title>
-    <link rel="stylesheet" type="text/css" href="css/media.css">
+    <link rel="stylesheet" type="text/css" href="./css/media.css">
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
     <meta charset="utf-8">
 </head>
 
 <body>
-
-    <div id="conteudo">
-        <header id="titulo">
-            Calculo de Médias
-        </header>
-
-        <div id="form">
-            <form name="frmMedia" method="post" action="media.php">
-                <div>
-                    <label>Nota 1:</label>
-                    <input type="text" name="txtn1" value="<?php echo ($nota1); ?>">
-                </div>
-
-                <div>
-                    <label>Nota 2:</label>
-                    <input type="text" name="txtn2" value="<?php echo ($nota2); ?>">
-                </div>
-
-                <div>
-                    <label>Nota 3:</label>
-                    <input type="text" name="txtn3" value="<?php echo ($nota3); ?>">
-                </div>
-
-                <div>
-                    <label>Nota 4:</label>
-                    <input type="text" name="txtn4" value="<?php echo ($nota4); ?>">
-                </div>
-                <div>
-                    <input type="submit" name="btncalc" value="Calcular">
-                    <div id="botaoReset">
-                        <a href="media.php">
-                            Novo Cálculo
+    <header id="">
+        <nav class="menu">
+            <div class="texto">
+                <h1></h1>
+                <a class="titulo">Cálculo Média</a>
+                <img src="imgs/divisao.png" alt="">
+                </h1>
+            </div>
+            <div class="container">
+                <input type="checkbox" id="checkbox-menu">
+                <label for="checkbox-menu">
+                    <span class="line1"></span>
+                    <span class="line2"></span>
+                    <span class="line3"></span>
+                </label>
+                <ul class="dropdown-menu" id="dropdown-menu">
+                    <li>
+                        <a href="index.html">
+                            <img src="imgs/casinha-de-cachorro (1).png" alt="">
                         </a>
-                    </div>
+                    </li>
+                    <li>
+                        <a href="calculadora_simples.php">
+                            <img src="imgs/calculadora (2).png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="media.php">
+                            <img src="imgs/divisao.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="tabuada.php">
+                            <img src="imgs/multiplicacao (1).png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="parimpar.php">
+                            <img src="imgs/vitoria.png" alt="">
+                            <img src="imgs/toque.png" alt="">
+                        </a>
+                    </li>
+                </ul>
+        </nav>
+    </header>
+    <div id="form">
+        <form name="frmMedia" method="post" action="media.php">
+            <div>
+                <label>Nota 1:</label>
+                <input type="text" name="txtn1" value="<?php echo ($nota1); ?>">
+            </div>
+
+            <div>
+                <label>Nota 2:</label>
+                <input type="text" name="txtn2" value="<?php echo ($nota2); ?>">
+            </div>
+
+            <div>
+                <label>Nota 3:</label>
+                <input type="text" name="txtn3" value="<?php echo ($nota3); ?>">
+            </div>
+
+            <div>
+                <label>Nota 4:</label>
+                <input type="text" name="txtn4" value="<?php echo ($nota4); ?>">
+            </div>
+            <div>
+                <input type="submit" name="btncalc" value="Calcular">
+                <div id="botaoReset">
+                    <a href="media.php">
+                        Novo Cálculo
+                    </a>
                 </div>
-            </form>
-        </div>
-        <footer id="resultado">
-            A média é:<?php echo ($media); ?>
-        </footer>
+            </div>
+        </form>
+    </div>
+    <footer id="resultado">
+        A média é:<?php echo ($media); ?>
+    </footer>
     </div>
 
 
